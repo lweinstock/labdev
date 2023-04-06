29f1b31e8c06b212a5cf820afa5e082f6a7b0ecb
Lars Steffen Weinstock <lars.steffen.weinstock@gmail.com>
2023-04-06 22:42:37 +0200

    Fixed overflow bug for stdev

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -8,8 +8,7 @@ using namespace std;
 
 namespace labdev {
 
-vector<string> split(string list, string delim,
-    size_t max_size) {
+vector<string> split(string list, string delim, size_t max_size) {
     vector<string> ret;
     size_t last = 0, pos = 0;
     while ( (pos = list.find(delim.c_str(), last)) != string::npos) {
@@ -28,34 +27,31 @@ bool equal(double a, double b, double epsilon) {
     return (abs(a-b) < epsilon);
 }
 
-template<typename T> double get_mean(vector<T> val)
+template<typename T> double get_mean(vector<T> vec)
 {
     // Very important: starting value has to be 0. and not 0
     // otherwise it treats everything as integers!!
-    double ret = accumulate(val.begin(), val.end(), 0.);
-    ret /= val.size();
-    return ret;
+    return accumulate(vec.begin(), vec.end(), 0.)/vec.size();
 }
 
-template double get_mean<double>(vector<double> val);
-template double get_mean<float>(vector<float> val);
-template double get_mean<int>(vector<int> val);
+template double get_mean<double>(vector<double> vec);
+template double get_mean<float>(vector<float> vec);
+template double get_mean<int>(vector<int> vec);
 
-template<typename T> double get_stdev(vector<T> val)
+template<typename T> double get_stdev(vector<T> vec)
 {
-    double v_mean = get_mean(val);
+    size_t n = vec.size();
+    double vec_mean = get_mean(vec);
     // Add up the squares using a lambda function
-    double v_mean2 = accumulate(val.begin(), val.end(), 0., 
-        [&](T a, T b) { return a + b*b; });
-    v_mean2 /= val.size();
-    double ret = sqrt( abs(v_mean2 - v_mean*v_mean) );
-    if (isnan(ret)) // Floating point precision..
-        return 0.;
-    return ret;
+    vector<double> dev(n);
+    transform(vec.begin(), vec.end(), dev.begin(), 
+        [&](double v) { return v - vec_mean; });
+    double var = inner_product(dev.begin(), dev.end(), dev.begin(), 0.)/n;
+    return sqrt(var);
 }
 
-template double get_stdev(vector<double> val);
-template double get_stdev(vector<float> val);
-template double get_stdev(vector<int> val);
+template double get_stdev(vector<double> vec);
+template double get_stdev(vector<float> vec);
+template double get_stdev(vector<int> vec);
 
 }
