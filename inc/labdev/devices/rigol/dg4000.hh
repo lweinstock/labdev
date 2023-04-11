38961f7dd5f573a2b9e039715f880ff60ca1ddeb
Lars Steffen Weinstock <lars.steffen.weinstock@gmail.com>
2023-04-11 09:46:14 +0200

    Started porting rigol DG4000 to new fgen API

---
+++ inc/labdev/devices/rigol/dg4000.hh
@@ -0,0 +1,94 @@
+#ifndef DG4000_H
+#define DG4000_H
+
+#include <labdev/tcpip_interface.hh>
+#include <labdev/visa_interface.hh>
+#include <labdev/usbtmc_interface.hh>
+#include <labdev/devices/fgen.hh>
+#include <labdev/protocols/scpi.hh>
+
+namespace labdev {
+
+/*
+ *      Rigol DG4000 series function generator
+ */
+
+class dg4000 : public fgen {
+public:
+    dg4000();
+    dg4000(ip_address &ip);
+    dg4000(visa_identifier &visa_id);
+    dg4000(usb_config &usb);
+    ~dg4000();
+
+    static constexpr uint16_t VID = 0x1AB1;
+    static constexpr uint16_t PID = 0x0641;
+    static constexpr unsigned PORT = 5555;
+
+    void connect(ip_address &ip);
+    void connect(visa_identifier &visa_id);
+    void connect(usb_config &usb);
+
+    // Turn channel on/off
+    void enable_channel(unsigned channel, bool ena = true) override;
+    // Get output state of channel (true = on, false = off)
+    bool get_state(unsigned channel) override;
+
+    // Set/get waveforms
+    void set_sine(unsigned channel,float freq_hz, float ampl_v = 1., 
+        float offset_v = 0., float phase_deg = 0.) override;
+    void set_square(unsigned channel,float freq_hz, float ampl_v = 1., 
+        float offset_v = 0., float phase_deg = 0., float duty_cycle = 0.5) override;
+    void set_ramp(unsigned channel,float freq_hz, float ampl_v = 1., 
+        float offset_v = 0., float phase_deg = 0., float symm = 0.5) override;
+    void set_pulse(unsigned channel,float period_s, float width_s, 
+        float delay_s = 0., float high_v = .1, float low_v = 0., 
+        float rise_s = 1e-6, float fall_s = 1e-6) override;
+    void set_noise(unsigned channel,float mean_v = 0.,
+        float stdev_v = 0.1) override;
+
+    bool is_sine(unsigned channel) override;
+    bool is_square(unsigned channel) override;
+    bool is_ramp(unsigned channel) override;
+    bool is_pulse(unsigned channel) override;
+    bool is_noise(unsigned channel) override;
+
+    // Signal frequency 
+    void set_freq(unsigned channel, float freq_hz) override;
+    float get_freq(unsigned channel) override;
+
+    // Signal duty cycle
+    void set_duty_cycle(unsigned channel, float dcl) override;
+    float get_duty_cycle(unsigned channel) override;
+
+    // Signal phase in degree
+    void set_phase(unsigned channel, float phase_deg) override;
+    float get_phase(unsigned channel) override;
+
+    // Signal amplitude in V
+    void set_ampl(unsigned channel, float ampl_v) override;
+    float get_ampl(unsigned channel) override;
+
+    // Signal offset in V
+    void set_offset(unsigned channel, float offset_v) override;
+    float get_offset(unsigned channel) override;
+
+private:
+    scpi* m_scpi;
+
+    void init();
+
+    // Aborts if channel is invalid
+    void check_channel(unsigned channel);
+
+    // Returns waveform string 
+    std::string get_waveform_str(unsigned channel);
+
+    // Returns when at least time_ms have passed after sending msg
+    void write_at_least(std::string msg, unsigned time_ms);
+
+};
+
+}
+
+#endif
---
+++ src/devices/rigol/dg4000.cpp
@@ -0,0 +1,331 @@
+#include <labdev/devices/rigol/dg4000.hh>
+#include <labdev/exceptions.hh>
+#include <labdev/ld_debug.hh>
+
+#include <sstream>
+#include <unistd.h>
+
+using namespace std;
+
+namespace labdev {
+
+dg4000::dg4000(): fgen(2, "Rigol,DG4000"), m_scpi(nullptr)
+{
+    return;
+}
+
+dg4000::dg4000(ip_address &ip): dg4000()
+{
+    this->connect(ip);
+    return;
+}
+
+dg4000::dg4000(visa_identifier &visa): dg4000()
+{
+    this->connect(visa);
+    return;
+}
+
+dg4000::dg4000(usb_config &usb): dg4000()
+{
+    this->connect(usb);
+    return;
+}
+
+dg4000::~dg4000() 
+{
+    if (m_scpi) {
+        delete m_scpi;
+        m_scpi = nullptr;
+    }
+    return;
+}
+
+void dg4000::connect(ip_address &ip)
+{
+    if ( this->connected() ) {
+        fprintf(stderr, "Device is already connected!\n");
+        abort();
+    }
+    if ( ip.port != dg4000::PORT ) {
+        fprintf(stderr, "DG4000 only supports port %i\n", dg4000::PORT);
+        abort();
+    }
+    m_comm = new tcpip_interface(ip);
+    m_scpi = new scpi(m_comm);
+    this->init();
+    return;
+}
+void dg4000::connect(visa_identifier &visa_id)
+{
+    if ( this->connected() ) {
+        fprintf(stderr, "Device is already connected!\n");
+        abort();
+    }
+    m_comm = new visa_interface(visa_id);
+    m_scpi = new scpi(m_comm);
+    this->init();
+    return;
+}
+void dg4000::connect(usb_config &usb)
+{
+    if ( this->connected() ) {
+        fprintf(stderr, "Device is already connected!\n");
+        abort();
+    }
+    usbtmc_interface* usbtmc = new usbtmc_interface(usb);
+    // USB initialization
+    usbtmc->claim_interface(0);
+    usbtmc->set_endpoint_in(0x08);
+    usbtmc->set_endpoint_out(0x02);
+    m_comm = usbtmc;
+    m_scpi = new scpi(m_comm);
+    this->init();
+    return;
+}
+
+void dg4000::enable_channel(unsigned channel, bool enable) 
+{
+    this->check_channel(channel);
+    stringstream msg("");
+    msg << ":OUTP" << channel << ":STAT" << (enable? " ON" : " OFF") << "\n";
+    m_comm->write(msg.str());
+    return;
+}
+
+bool dg4000::get_state(unsigned channel)
+{
+    return false;
+}
+
+void dg4000::set_sine(unsigned channel,float freq_hz, float ampl_v, 
+    float offset_v, float phase_deg)
+{
+    this->check_channel(channel);
+    stringstream msg;
+    msg << ":SOUR" << channel << ":APPL:SIN ";
+    msg << freq_hz << ",";
+    msg << ampl_v << ","; 
+    msg << offset_v << ",";
+    msg << phase_deg << "\n";
+    //m_comm->write_at_least(msg.str(), 10);
+    m_comm->write(msg.str());
+    m_scpi->wait_to_complete();
+    return;
+}
+
+void dg4000::set_square(unsigned channel,float freq_hz, float ampl_v, 
+    float offset_v, float phase_deg, float duty_cycle)
+{
+    return;
+}
+
+void dg4000::set_ramp(unsigned channel,float freq_hz, float ampl_v, 
+    float offset_v, float phase_deg, float symm)
+{
+    return;
+}
+
+void dg4000::set_pulse(unsigned channel,float period_s, float width_s, 
+    float delay_s, float high_v, float low_v, float rise_s, float fall_s)
+{
+    return;
+}
+
+void dg4000::set_noise(unsigned channel,float mean_v, float stdev_v)
+{
+    return;
+}
+
+// TODO: test get waveform mehtods
+
+bool dg4000::is_sine(unsigned channel)
+{
+    if (this->get_waveform_str(channel) == "SINUSOID")
+        return true;
+    return false;
+}
+
+bool dg4000::is_square(unsigned channel)
+{
+    if (this->get_waveform_str(channel) == "SQUARE")
+        return true;
+    return false;
+}
+
+bool dg4000::is_ramp(unsigned channel)
+{
+    if (this->get_waveform_str(channel) == "RAMP")
+        return true;
+    return false;
+}
+
+bool dg4000::is_pulse(unsigned channel)
+{
+    if (this->get_waveform_str(channel) == "PULSE")
+        return true;
+    return false;
+}
+
+bool dg4000::is_noise(unsigned channel)
+{
+    if (this->get_waveform_str(channel) == "NOISE")
+        return true;
+    return false;
+}
+
+void dg4000::set_freq(unsigned channel, float freq_hz)
+{
+    this->check_channel(channel);
+    if (freq_hz < 0) {
+        fprintf(stderr, "Invalid frequency %f\n", freq_hz);
+        abort();
+    }
+    stringstream msg("");
+    msg << ":SOUR" << channel << ":FREQ " << freq_hz << "\n";
+    this->write_at_least(msg.str(), 5);
+    return;
+}
+
+float dg4000::get_freq(unsigned channel)
+{
+    this->check_channel(channel);
+    stringstream msg("");
+    msg << ":SOUR" << channel << ":FREQ?\n";
+    string resp = m_comm->query(msg.str());
+    return stof(resp);
+}
+
+void dg4000::set_duty_cycle(unsigned channel, float dcl)
+{
+    this->check_channel(channel);
+    if (dcl < 0. || dcl > 1.) {
+        fprintf(stderr, "Invalid duty cycle %f\n", dcl);
+        abort();
+    }
+    stringstream msg("");
+    msg << ":SOUR" << channel << ":PULS:DCYC " << 100*dcl << "\n";
+    this->write_at_least(msg.str(), 5);
+    return;
+}
+
+float dg4000::get_duty_cycle(unsigned channel)
+{
+    return 0.;
+}
+
+void dg4000::set_phase(unsigned channel, float phase_deg)
+{
+    this->check_channel(channel);
+    if (phase_deg < 0. || phase_deg > 360.) {
+        fprintf(stderr, "Invalid phase %f\n", phase_deg);
+        abort();
+    }
+    stringstream msg("");
+    msg << ":SOUR" << channel << ":PHAS " << phase_deg << "\n";
+    this->write_at_least(msg.str(), 5);
+    return;
+}
+
+float dg4000::get_phase(unsigned channel)
+{
+    this->check_channel(channel);
+    stringstream msg("");
+    msg << ":SOUR" << channel << ":PHAS?\n";
+    string resp = m_comm->query(msg.str());
+    return stof(resp);
+}
+
+void dg4000::set_ampl(unsigned channel, float ampl_v)
+{
+    this->check_channel(channel);
+    if (ampl_v < 0. || ampl_v > 20.) {
+        fprintf(stderr, "Amplitude %f out of range\n", ampl_v);
+        abort();
+    }
+    stringstream msg("");
+    msg << ":SOUR" << channel << ":VOLT " << ampl_v << "\n";
+    this->write_at_least(msg.str(), 5);
+    return;
+}
+
+float dg4000::get_ampl(unsigned channel)
+{
+    this->check_channel(channel);
+    stringstream msg("");
+    msg << ":SOUR" << channel << ":VOLT?\n";
+    string resp = m_comm->query(msg.str());
+    return stof(resp);
+}
+
+void dg4000::set_offset(unsigned channel, float offset_v)
+{
+    this->check_channel(channel);
+    if (offset_v < -7.5 || offset_v > 7.5) {
+        fprintf(stderr, "Offset %f out of range\n", offset_v);
+        abort();
+    }
+    stringstream msg("");
+    msg << ":SOUR" << channel << ":VOLT:OFFS " << offset_v << "\n";
+    this->write_at_least(msg.str(), 5);
+    return;
+}
+
+float dg4000::get_offset(unsigned channel)
+{
+    this->check_channel(channel);
+    stringstream msg("");
+    msg << ":SOUR" << channel << ":VOLT:OFFS?\n";
+    string resp = m_comm->query(msg.str());
+    return stof(resp);
+}
+
+/*
+ *      P R I V A T E   M E T H O D S
+ */
+
+void dg4000::init() 
+{
+    m_scpi->clear_status();
+    m_scpi->wait_to_complete();
+    return;
+}
+
+void dg4000::check_channel(unsigned channel) 
+{
+    if ( (channel == 0) || (channel > this->get_n_channels()) ) {
+        fprintf(stderr, "Invalid channel %i\n", channel);
+        abort();
+    }
+    return;
+}
+
+std::string dg4000::get_waveform_str(unsigned channel)
+{
+    this->check_channel(channel);
+    string resp = m_comm->query(":SOUR" + to_string(channel) + ":APPL?\n");
+    string waveform = resp.substr(resp.find_first_of(','));
+    return waveform;
+}
+
+void dg4000::write_at_least(string msg, unsigned time_ms) {
+
+    /*
+     * Known issue: while processing queries the DG4000 apparently ignores
+     *  all following queries instead of storing them in an internal m_command
+     *  queue. This makes it neccesary for some write m_commands to take at
+     *  least a few milliseconds before returning.
+     */
+
+    struct timeval sta, sto;
+    gettimeofday(&sta, NULL);
+    m_comm->write(msg);
+    gettimeofday(&sto, NULL);
+
+    int diff_ms = (sto.tv_sec-sta.tv_sec)*1000 + (sto.tv_usec-sta.tv_usec)/1000;
+    if (time_ms > diff_ms)
+        usleep( (time_ms - diff_ms)*1000 );
+    return;
+}
+
+}
